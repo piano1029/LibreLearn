bedbcaa0751a6096d1e3ae3dca84bb5d08cb1d29
piano1029 <46605242+piano1029@users.noreply.github.com>
2022-09-23 21:00:29 +0200

    resize the add new item thing

--- src/components/SetCreatorAddItem.tsx
+++ src/components/SetCreatorAddItem.tsx
@@ -4,7 +4,7 @@ import { SetItem } from '../api/sets';
 
 export function SetCreatorAddItem({ addEmpty }: { addEmpty: (item: any) => void }) {
     return (
-        <ActionIcon color="blue" size="xl" variant="filled" onClick={addEmpty} >
+        <ActionIcon color="blue" size="sm" variant="filled" onClick={addEmpty} >
             <IconPlus size={34} />
         </ActionIcon>
     );

4e1908dfd4b60f5499694f2a45646f440c079c67
piano1029 <46605242+piano1029@users.noreply.github.com>
2022-10-01 11:40:46 +0200

    Add results viewer

--- src/studyMethods/FlashCards.tsx
+++ src/studyMethods/FlashCards.tsx
@@ -2,7 +2,7 @@ import { IconStack2, TablerIcon } from "@tabler/icons";
 import { SerializedSet, SetItem } from "../api/sets";
 import { StudyMethod } from "../api/studyMethods";
 import { Carousel } from '@mantine/carousel';
-import { Button, Card, Group, Stack } from "@mantine/core";
+import { Button, Card, Grid, Group, Progress, Stack, Table, createStyles, Text } from "@mantine/core";
 import React, { useState } from "react";
 
 type FlashCardsGameData = {
@@ -59,10 +59,20 @@ function FlashCard({ item, setState }: { item: SetItem, setState: (state: boolea
     </>
 }
 
+const useStyles = createStyles((theme) => ({
+    progressBar: {
+        '&:not(:first-of-type)': {
+            borderLeft: `3px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white}`,
+        },
+    },
+}));
+
 const FlashCards: StudyMethod = {
     name: `FlashCards`,
     icon: IconStack2,
     Render: ({ set }) => {
+        const { classes, cx, theme } = useStyles();
+
         let [counter, setCount] = useState(0)
         let [gameData, setGameData] = useState<FlashCardsGameData>({
             items: set.items.map((item) => {
@@ -73,6 +83,50 @@ const FlashCards: StudyMethod = {
                 }
             })
         })
+
+        if (gameData.items[counter] === undefined) {
+            return <Grid>
+                <Grid.Col span={12}><h1 style={{ textAlign: 'center', width: '100%' }} >Results</h1></Grid.Col>
+                <Grid.Col span={12}><Table sx={{ minWidth: 700 }}>
+                    <thead >
+                        <tr>
+                            <th>Left</th>
+                            <th>Right</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>{gameData.items.map((value) => {
+                        return <tr key={value.item.uuid} >
+                            <td dangerouslySetInnerHTML={{ __html: value.item.left.startsWith(`<p`) ? value.item.left : `<p>${value.item.left}</p>` }} />
+                            <td dangerouslySetInnerHTML={{ __html: value.item.right.startsWith(`<p`) ? value.item.right : `<p>${value.item.right}</p>` }} />
+                            <td>
+                                <Group position="apart">
+                                    <Text size="xs" color="teal" weight={700}>
+                                        {((value.timesSuccess / (value.timesFailure + value.timesSuccess)) * 100).toFixed(0)}%
+                                    </Text>
+                                    <Text size="xs" color="red" weight={700}>
+                                        {((value.timesFailure / (value.timesFailure + value.timesSuccess)) * 100).toFixed(0)}%
+                                    </Text>
+                                </Group>
+                                <Progress
+                                    classNames={{ bar: classes.progressBar }}
+                                    sections={[
+                                        {
+                                            value: (value.timesSuccess / (value.timesFailure + value.timesSuccess)) * 100,
+                                            color: theme.colorScheme === 'dark' ? theme.colors.teal[9] : theme.colors.teal[6],
+                                        },
+                                        {
+                                            value: (value.timesFailure / (value.timesFailure + value.timesSuccess)) * 100,
+                                            color: theme.colorScheme === 'dark' ? theme.colors.red[9] : theme.colors.red[6],
+                                        },
+                                    ]}
+                                />
+                            </td>
+                        </tr>
+                    })}</tbody>
+                </Table></Grid.Col>
+            </Grid>
+        }
 
         return <Group style={{ width: '200px' }} onScroll={(e) => { e.preventDefault() }} >
             {gameData.items[counter] !== undefined ? <FlashCard setState={(state) => {
